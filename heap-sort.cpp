c5eb9a6a70edb0427ac85be5acea37c735c96ce1
linda cai <basket6011@hotmail.com>
2016-04-29 22:46:56 -0700

    build heap before sorting

---
+++ heap-sort.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <string>
+#include <cstdio>
+
+using namespace std;
+
+int size = 0;
+
+// fill array with user inputs
+void creatElement(int array[]){
+	for(int i = 0; i < size; i++){
+		cin >> array[i];
+	}
+}
+
+// swap array elements
+void swap(int* big, int* small){
+	int temp = *big;
+	*big = *small;
+	*small = temp;
+}
+
+
+/*
+lchild, rchild: index of the left and right child
+return the index of the smaller child
+*/
+int min(int array[], int lchild, int rchild){
+	if(array[lchild] < array[rchild]) return lchild;
+	else return rchild;
+}
+
+
+/*
+compare current element with its children
+return the index of the smaller child
+if not exist, return negtive
+*/
+int compare(int array[], int index, int lchild, int rchild){
+	if(rchild >= 0){
+		if(array[index] > array[lchild] && array[index] > array[rchild]) return min(array, lchild, rchild);
+		if(array[index] > array[rchild]) return rchild;
+	}
+	if(array[index] > array[lchild]) return lchild;
+	else return -1;
+}
+
+// swap down elements to maintain min heap feature
+void sinkDown(int array[], int index){
+	if(index > (size/2 - 1)) return;
+	else{
+		int lchild = 2*index + 1;
+		int rchild = (2*index + 2 > size - 1)? -1 : 2*index + 2;   // when size is even -> out of boundary
+		int child_index = compare(array, index, lchild, rchild);
+		if(child_index >= 0){
+			swap(&array[index], &array[child_index]);
+			sinkDown(array, child_index);
+		}
+		else return;
+	}
+}
+
+// build heap starting from the second last level
+void buildHeap(int array[]){
+	for(int i = (size/2 - 1); i >= 0; i--){
+		sinkDown(array, i);
+	}
+}
+
+// sort array in increasing order
+/*void sort(int array[]){
+	bool swapped = false;
+	do{
+		swapped = false;
+		for(int i = 0; i < size - 1; i++){
+			if(array[i] > array[i+1]){
+				swap(&array[i], &array[i+1]);
+				swapped = true;
+			}
+		}	
+	}while(swapped);
+
+}*/
+
+// print out sorted array
+void print(int array[]){
+	for(int i = 0; i < size; i++){
+		printf("%d ", array[i]);
+	}
+}
+
+int main(){
+	int numOfele;
+	printf("Enter number of elements: ");
+	cin >> numOfele;
+	int array[numOfele];
+	size = numOfele;
+
+	printf("Enter %d integers:\n", numOfele);
+	creatElement(array);
+
+	printf("Build heap:\n");
+	buildHeap(array);
+	print(array);
+
+	printf("\nStart:\n");
+	//sort(array);
+	//print(array);
+	printf("\nDone");
+}
