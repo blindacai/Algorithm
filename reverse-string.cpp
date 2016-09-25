b1716fc7c2fff90a8cf9cd3086c13fa1f3d6eb9a
linda cai <basket6011@hotmail.com>
2016-09-25 15:27:06 -0700

    in place reverse of a string

---
+++ reverse-string.cpp
@@ -0,0 +1,20 @@
+// input: {'a','b','c'} or "abc"
+void reverse(char* str){
+    char* end = str;
+    char tmp;
+
+    if(str){
+        while(*end){
+            end += 1;
+        }
+        end -= 1;
+
+        while(str < end){
+            tmp = *str;
+            *str = *end;
+            *end = tmp;
+            str += 1;
+            end -= 1;
+        }
+    }
+}
