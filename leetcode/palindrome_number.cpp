c1b8ceb7ed4cde490753bc9677415b041a021a12
blindacai <linda_cai2014@hotmail.com>
2017-05-09 23:54:38 -0700

    palindrome number

---
+++ leetcode/palindrome_number.cpp
@@ -0,0 +1,26 @@
+// 3. Palindrome Number
+
+// Determine whether an integer is a palindrome. Do this without extra space.
+
+# include <iostream>
+#include <stdlib.h>
+
+bool isPalindrome(int x) {
+    int org = abs(x);
+    int reverse = 0;
+    while(x != 0){
+        reverse = reverse * 10 + x % 10;
+        x = x / 10;
+    }
+
+    if((reverse - org) == 0){
+        return true;
+    }
+    else return false;
+}
+
+int main(){
+    if(isPalindrome(-12321)){
+        std::cout << "yes\n";
+    }
+}
--- leetcode/reverse_int.cpp
+++ leetcode/reverse_int.cpp
@@ -14,6 +14,8 @@ The input is assumed to be a 32-bit signed integer. Your function should return 
 # include <vector>
 #include <math.h>
 
+const int INT_MAX = 2147483647;
+const int INT_MIN = -2147483648;
 
 int build_int(std::vector<int>& int_list){
     long result = 0;
@@ -22,14 +24,14 @@ int build_int(std::vector<int>& int_list){
         result += int_list[i] * pow(10, len-i-1);
     }
 
-    if(result > 2147483647 || result < -2147483648){
+    if(result > INT_MAX || result < INT_MIN){
         return 0;
     }
     else return result;
 }
 
 int reverse(int x) {
-    std::vector<int> digits;
+    std::vector<int> digits;   // initialize without knowing size
 
     while(x != 0){
         digits.push_back(x - (x / 10) * 10);
@@ -39,6 +41,20 @@ int reverse(int x) {
     return build_int(digits);
 }
 
+int reverse1(int x){
+    long res = 0;
+    while(x != 0){
+        res = res * 10 + x % 10;
+        x = x / 10;
+
+        if(res > INT_MAX || res < INT_MIN){
+            return 0;
+        }     
+    }
+    return res;
+}
+
 int main(){
-    std::cout << reverse(1234567893);
+    std::cout << reverse(1234567893) << '\n';
+    std::cout << reverse1(1234567893) << '\n';
 }
